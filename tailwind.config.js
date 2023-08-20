66bd4994f7e4e70dd11ebeb8edb53cbcab3411be
Rangga Adithia <rangga.adithia26@gmail.com>
2023-08-20 11:31:46 +0800

    initial commit for template

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    // colors: {
+    //   primary: '#22c55e',
+    //   secondary: '#86efac'
+    // },
+    extend: {
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
