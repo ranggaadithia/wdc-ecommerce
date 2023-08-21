f4312f5b4bf8e7d8c82215235b4c32f856a16043
Rangga Adithia <rangga.adithia26@gmail.com>
2023-08-21 14:04:47 +0800

    finish search bar

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// search suggest pop-up
 const searchBar = document.getElementById('search-bar')
 const searchSuggest = document.getElementById('search-suggest')
 
@@ -6,10 +7,29 @@ searchBar.addEventListener('click', function(event) {
  event.stopPropagation();
 })
 
+const backSearch = document.getElementById("back-search")
+searchBar.addEventListener('click', function(event) {
+  backSearch.classList.remove('hidden')
+})
+backSearch.addEventListener('click', e => {
+  backSearch.classList.add('hidden')
+})
+
 document.addEventListener('click', function(event) {
  if (!searchSuggest.contains(event.target)) {
    searchSuggest.classList.add('hidden');
  }
+});
+
+
+// deleting history
+const historyItems = document.querySelectorAll('#history-list');
+const closeButtons = document.querySelectorAll('#close-history-button');
+
+closeButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    historyItems[index].classList.add('hidden');
+  });
 });
 
 
