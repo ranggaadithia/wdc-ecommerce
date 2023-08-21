69726ea73cbbad9f26d16dc8ea61531b1afd1714
Rangga Adithia <rangga.adithia26@gmail.com>
2023-08-21 13:04:24 +0800

    replace boostrap grid

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,14 @@ const searchBar = document.getElementById('search-bar')
 const searchSuggest = document.getElementById('search-suggest')
 
 searchBar.addEventListener('click', function(event) {
- searchSuggest.classList.remove('d-none')
+ searchSuggest.classList.remove('hidden')
  event.stopPropagation();
 })
 
 document.addEventListener('click', function(event) {
  if (!searchSuggest.contains(event.target)) {
-   searchSuggest.classList.add('d-none');
+   searchSuggest.classList.add('hidden');
  }
 });
+
+
